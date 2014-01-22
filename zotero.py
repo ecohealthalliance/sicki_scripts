2bdab88502347191acda67652f44c490a13bd91f
Nico Preston <nicopresto@gmail.com>
2014-01-22 03:09:05 -0500

    updates zotero for new collections and add py gitignore

--- zotero.py
+++ zotero.py
@@ -6,14 +6,15 @@ from getpass import getpass
 from sys import argv
 
 if __name__ == '__main__':
-    passwd = getpass ("Password: ")
-    mongo = pymongo.Connection ('localhost', 27017)['db_skapes']
-    mongo.authenticate('skapes', passwd)
+    #passwd = getpass ("Password: ")
+    mongo = pymongo.Connection ('localhost', 27017)['refs'] #test
+    #mongo = pymongo.Connection('localhost', 3002)['meteor']
+    #mongo.authenticate('skapes', passwd)
 
-    library_id= '77783'
+    library_id= '126319'
     library_type='group'
-    api_key = 'hT2SLlqonWyD1253s93zg3bC'
+    api_key = 'TwxmBGN2hCcTrKePkXPaX9RI'
     zot = zotero.Zotero(library_id, library_type, api_key)
     items = zot.all_top ()
     for item in items:
-        mongo.refs.insert (item)
+        mongo.references.insert (item)
